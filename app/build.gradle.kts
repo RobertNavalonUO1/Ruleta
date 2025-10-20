12907f2c50265a4b656e32c5e1b19db01f754370
IngridDiazRisso <idiazris@uoc.edu>
2025-10-21 00:42:17 +0200

    Añadido tablero

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,14 @@ android {
         versionName = "1.0"
     }
 
-    buildFeatures { viewBinding = true }
+    buildFeatures {
+        viewBinding = true
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.14"
+    }
+
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
@@ -29,6 +36,11 @@ dependencies {
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.activity:activity-compose:1.8.0")
+    implementation("androidx.compose.ui:ui:1.5.3")
+    implementation("androidx.compose.material3:material3:1.2.0")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.5.3")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.5.3")
 
     val navVersion = "2.8.2"
     implementation("androidx.navigation:navigation-fragment-ktx:$navVersion")
