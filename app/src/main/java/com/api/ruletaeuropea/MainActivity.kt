0d564e20aa20f98cc949f2d430beeb703f99ab5f
Andrea <amorenocir@uoc.edu>
2025-10-21 22:43:00 +0200

    Añadir SplashActivity y mainActivity para evitar crash en ejecución

---
+++ app/src/main/java/com/api/ruletaeuropea/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.api.ruletaeuropea
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main) // crea un layout activity_main.xml
+    }
+}
