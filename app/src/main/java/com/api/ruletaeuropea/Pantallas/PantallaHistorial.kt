98c29b22b67e96db8dbec7c33202222d0b7ade1e
Andrea <amorenocir@uoc.edu>
2025-11-02 17:39:24 +0100

    Implementación interfaz grafica pantalla Historial

--- app/src/main/java/com/api/ruletaeuropea/Pantallas/PantallaHistorial.kt
+++ app/src/main/java/com/api/ruletaeuropea/Pantallas/PantallaHistorial.kt
@@ -1,16 +1,25 @@
 package com.api.ruletaeuropea.pantallas
-
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.api.ruletaeuropea.App
 import com.api.ruletaeuropea.data.entity.Historial
 
@@ -20,14 +29,71 @@ fun PantallaHistorial(jugadorNombre: String) {
     val historialFlow = dao.verHistorial(jugadorNombre)
     val lista by historialFlow.collectAsState(initial = emptyList())
 
-    Column(modifier = Modifier.fillMaxSize()) {
-        Text("Historial de ${jugadorNombre}")
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF1E1E1E))
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        //Encabezado
+        Text(
+            "Historial de ${jugadorNombre}",
+            fontSize = 28.sp,
+            fontWeight = FontWeight.ExtraBold,
+            color = Color(0xFFFFFFFF),
+            modifier = Modifier
+                .padding(bottom = 16.dp)
+                .align(Alignment.CenterHorizontally)
+        )
+
         if (lista.isEmpty()) {
-            Text("No hay registros")
+            Text(
+                "No hay registros",
+                fontSize = 18.sp,
+                color = Color.LightGray,
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            )
         } else {
-            LazyColumn(contentPadding = PaddingValues(16.dp)) {
+            LazyColumn(
+                verticalArrangement = Arrangement.spacedBy(12.dp),
+                contentPadding = PaddingValues(16.dp)
+            ) {
                 items(lista) { item: Historial ->
-                    Text(text = "Apuesta #${item.NumApuesta} - Resultado: ${item.Resultado} - Saldo: ${item.SaldoDespues}")
+                    Card(
+                        shape = RoundedCornerShape(12.dp),
+                        colors = CardDefaults.cardColors(
+                            containerColor = Color.Gray.copy(alpha = 0.2f)
+                        ),
+                        modifier = Modifier.fillMaxWidth(0.6f)
+
+                    ) {
+                        Row(
+                            modifier = Modifier
+                                .padding(16.dp)
+                                .fillMaxWidth(),
+                            horizontalArrangement = Arrangement.SpaceBetween, // espacio entre los elementos
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Text(
+                                text = "Apuesta #${item.NumApuesta}",
+                                fontWeight = FontWeight.Bold,
+                                fontSize = 20.sp,
+                                color = Color.White
+                            )
+                            Text(
+                                text = "Resultado: ${item.Resultado}",
+                                fontSize = 20.sp,
+                                color = Color.White
+                            )
+                            Text(
+                                text = "Saldo: ${item.SaldoDespues} C",
+                                fontSize = 20.sp,
+                                color = Color.White
+                            )
+                        }
+                    }
                 }
             }
         }
